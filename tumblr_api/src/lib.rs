a0d7a127b4293bb628bdb42b7c9a5f4695f230a6
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-08-12 19:25:50 -0700

    add feature flags, remove unused deps

--- tumblr_api/src/lib.rs
+++ tumblr_api/src/lib.rs
@@ -1,5 +1,8 @@
 //! A (currently incomplete) rust implementation of the Tumblr API.
 
+#[cfg(feature = "api")]
 pub mod api;
+#[cfg(feature = "client")]
 pub mod client;
+#[cfg(feature = "npf")]
 pub mod npf;
