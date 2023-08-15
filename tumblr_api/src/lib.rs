a93dad03420b36cea46751ba0b0ef7a73a96a0b2
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-08-15 15:57:19 -0700

    add some more top level docs

--- tumblr_api/src/lib.rs
+++ tumblr_api/src/lib.rs
@@ -1,4 +1,30 @@
 //! A (currently incomplete) rust implementation of the Tumblr API.
+//!
+//! # Examples
+//!
+//! ## Creating a simple post with the client
+//!```no_run
+//! # #[tokio::main]
+//! # async fn main() -> anyhow::Result<()> {
+//! use tumblr_api::client::{Client, Credentials};
+//! use tumblr_api::npf;
+//! let client = Client::new(Credentials::new_oauth2(
+//!     "your consumer key",
+//!     "your consumer secret",
+//! ));
+//! client
+//!     .create_post(
+//!         "blog-name",
+//!         vec![npf::ContentBlockText::builder("hello world").build()],
+//!     )
+//!     .send()
+//!     .await?;
+//! # Ok(())
+//! # }
+//! ```
+//!
+//! # Modules & Feature Flags
+//! This library is split into 3 modules - `client`, `api`, and `npf` - and each has a feature flag of the same name that controls whether it's enabled.
 
 #[cfg(feature = "api")]
 pub mod api;
