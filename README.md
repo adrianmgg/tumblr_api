0b9e5d9abb1767abfc5853f7267d8ea9d7af1542
amgg <16143061+adrianmgg@users.noreply.github.com>
2023-11-18 16:13:31 -0800

    add a more complex example for create_post
    including some notes about the things that aren't finalized about that api

--- README.md
+++ README.md
@@ -26,6 +26,46 @@ client
     .await?;
 ```
 
+### Creating a more complex post
+```rust
+use tumblr_api::client::CreatePostState;
+// load the image that we'll be attaching to the post.
+let my_image = std::fs::read("path/to/my_image.jpg")?;
+// (currently, you need to manually create the reqwest::Body to pass in. that'll probably
+//  change in a future version.)
+let my_image = reqwest::Body::from(my_image);
+client
+    .create_post(
+        "blog-name",
+        vec![
+            npf::ContentBlockText::builder("hello world").build(),
+            npf::ContentBlockImage::builder(vec![npf::MediaObject::builder(
+                npf::MediaObjectContent::Identifier("my-image-identifier".into()),
+            )
+            .build()])
+            .build(),
+            npf::ContentBlockText::builder("some bold text in a heading")
+                .subtype(npf::TextSubtype::Heading1)
+                .formatting(vec![npf::InlineFormat {
+                    start: 5,
+                    end: 9,
+                    format: npf::InlineFormatType::Bold,
+                }])
+                .build(),
+        ],
+    )
+    .add_attachment(my_image, "image/jpeg", "my-image-identifier")
+    // add tags to your post
+    // (this is currently a string since that's what the underlying api takes.
+    //  Being able to pass a Vec<String> instead is a planned feature but hasn't been
+    //  implemented quite yet.)
+    .tags("tag_1,tag_2,tag_3")
+    // add the post to your queue instead of immediately posting it
+    .initial_state(CreatePostState::Queue)
+    .send()
+    .await?;
+```
+
 ## Modules & Feature Flags
 This library is split into 3 modules - `client`, `api`, and `npf` - and each has a feature flag of the same name that controls whether it's enabled.
 
--- tumblr_api/src/lib.rs
+++ tumblr_api/src/lib.rs
@@ -1,5 +1,5 @@
 //! A rust implementation of the Tumblr API.
-//! 
+//!
 //! This is still very much in beta! see [Major Planned/Unimplemented Features](#major-plannedunimplemented-features)
 //!
 //! # Examples
@@ -25,9 +25,59 @@
 //! # }
 //! ```
 //!
+//! ## Creating a more complex post
+//! ```no_run
+//! # #[tokio::main]
+//! # async fn main() -> anyhow::Result<()> {
+//! # use tumblr_api::client::{Client, Credentials};
+//! # use tumblr_api::npf;
+//! use tumblr_api::client::CreatePostState;
+//! # let client = Client::new(Credentials::new_oauth2(
+//! #     "your consumer key",
+//! #     "your consumer secret",
+//! # ));
+//! // load the image that we'll be attaching to the post.
+//! let my_image = std::fs::read("path/to/my_image.jpg")?;
+//! // (currently, you need to manually create the reqwest::Body to pass in. that'll probably
+//! //  change in a future version.)
+//! let my_image = reqwest::Body::from(my_image);
+//! client
+//!     .create_post(
+//!         "blog-name",
+//!         vec![
+//!             npf::ContentBlockText::builder("hello world").build(),
+//!             npf::ContentBlockImage::builder(vec![npf::MediaObject::builder(
+//!                 npf::MediaObjectContent::Identifier("my-image-identifier".into()),
+//!             )
+//!             .build()])
+//!             .build(),
+//!             npf::ContentBlockText::builder("some bold text in a heading")
+//!                 .subtype(npf::TextSubtype::Heading1)
+//!                 .formatting(vec![npf::InlineFormat {
+//!                     start: 5,
+//!                     end: 9,
+//!                     format: npf::InlineFormatType::Bold,
+//!                 }])
+//!                 .build(),
+//!         ],
+//!     )
+//!     .add_attachment(my_image, "image/jpeg", "my-image-identifier")
+//!     // add tags to your post
+//!     // (this is currently a string since that's what the underlying api takes.
+//!     //  Being able to pass a Vec<String> instead is a planned feature but hasn't been
+//!     //  implemented quite yet.)
+//!     .tags("tag_1,tag_2,tag_3")
+//!     // add the post to your queue instead of immediately posting it
+//!     .initial_state(CreatePostState::Queue)
+//!     .send()
+//!     .await?;
+//! # Ok(())
+//! # }
+//! ```
+//!
 //! # Modules & Feature Flags
 //! This library is split into 3 modules - `client`, `api`, and `npf` - and each has a feature flag of the same name that controls whether it's enabled.
-//! 
+//!
 //! # Major Planned/Unimplemented Features
 //! - refreshing access tokens (currently, the client will just start failing after the token expires)
 //! - implement remaining api endpoints (currently it's just post creation plus a couple others)
